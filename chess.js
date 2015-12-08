d90a5e2e66ce6e39e74ff48776da91a8a24a967b
philmphoenix <jpholzmann@gmail.com>
2015-12-07 20:30:49 -0700

    Adds loading pieces and constructors for pieces.

--- chess.js
+++ chess.js
@@ -8,16 +8,15 @@ var board = {
 
 loadBoard();
 loadSquares();
+loadPieces();
 
 function doMouseDown (event) {
   canvas_x = event.pageX;
   canvas_y = event.pageY;
   var indexOfSquare = (Math.floor(canvas_y / 100) * 8) + Math.floor(canvas_x / 100)
   var square = board.squares[indexOfSquare]
-  alert("X = " + square.xCoordinate + "Y = " + square.yCoordinate)
-}
-
-
+  alert("X = " + square.xCoordinate + "Y = " + square.yCoordinate + " " + square.piece.color + " " + square.piece.constructor.name)
+};
 
 function loadBoard () {
   for (var i = 0; i < 4; i++) {
@@ -39,9 +38,80 @@ function loadSquares () {
   };
 };
 
-
 function Square (xCoordinate, yCoordinate) {
   this.xCoordinate = xCoordinate;
   this.yCoordinate = yCoordinate;
   this.piece = null;
+};
+
+function loadPieces () {
+  loadBlack();
+  loadWhite();
+};
+
+function Piece (color) {
+  this.color = color;
+};
+
+function Rook (color) {
+  this.color = color;
+  this.prototype = Piece;
+};
+
+function Knight (color) {
+  this.color = color;
+  this.prototype = Piece;
+};
+
+function Bishop (color) {
+  this.color = color;
+  this.prototype = Piece;
+};
+
+function Queen (color) {
+  this.color = color;
+  this.prototype = Piece;
+};
+
+function King (color) {
+  this.color = color;
+  this.prototype = Piece;
+};
+
+function WhitePawn (color) {
+  this.color = color;
+  this.prototype = Piece;
+};
+
+function BlackPawn (color) {
+  this.color = color;
+  this.prototype = Piece;
+};
+
+function loadBlack () {
+  board.squares[0].piece = new Rook ("black");
+  board.squares[1].piece = new Knight ("black");
+  board.squares[2].piece = new Bishop ("black");
+  board.squares[3].piece = new Queen ("black");
+  board.squares[4].piece = new King ("black");
+  board.squares[5].piece = new Bishop ("black");
+  board.squares[6].piece = new Knight ("black");
+  board.squares[7].piece = new Rook ("black");
+  for (var i = 8; i < 16; i++) {
+    board.squares[i].piece = new BlackPawn ("black");
+  }
+};
+
+function loadWhite () {
+  for (var i = 48; i < 56; i++) {
+    board.squares[i].piece = new WhitePawn ("white");
+  }
+  board.squares[56].piece = new Rook ("white");
+  board.squares[57].piece = new Knight ("white");
+  board.squares[58].piece = new Bishop ("white");
+  board.squares[59].piece = new Queen ("white");
+  board.squares[60].piece = new King ("white");
+  board.squares[61].piece = new Bishop ("white");
+  board.squares[62].piece = new Knight ("white");
+  board.squares[63].piece = new Rook ("white");
 };
